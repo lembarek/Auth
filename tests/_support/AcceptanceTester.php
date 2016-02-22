03057cafa96955033014dddf74765f8850229d68
lembarek <lembarek2011@gmail.com>
2016-02-22 03:21:55 +0100

    add acceptance tests

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -23,4 +23,49 @@ class AcceptanceTester extends \Codeception\Actor
    /**
     * Define custom actions here
     */
+
+    /**
+     * it register a user
+     *
+     * @param  array  $overrides
+     * @return void
+     */
+    public function register(array $overs=[])
+    {
+        $name = 'joe';
+        $email = 'joe@example.com';
+        $password = 'secret';
+
+        extract($overs);
+
+        $this->amOnPage('/register');
+        $this->fillField('username',$name);
+        $this->fillField('email',$email);
+        $this->fillField('password',$password);
+        $this->fillField('password_confirmation',$password);
+        $this->click('input[type=submit]');
+    }
+
+    /**
+     * it login a user
+     *
+     * @param  array  $overs
+     * @return void
+     */
+    public function login(array $overs=[])
+    {
+        $email = 'joe@example.com';
+        $password = 'secret';
+
+        extract($overs);
+
+        $this->amOnPage('/login');
+        $this->fillField('email', $email);
+        $this->fillField('password', $password);
+        $this->click('input[type=submit]');
+    }
+
+
+
+
 }
--- tests/acceptance/AuthControllerCest.php
+++ tests/acceptance/AuthControllerCest.php
@@ -11,20 +11,43 @@ class AuthControllerCest
     {
     }
 
-    public function it_register_a_valide_user(FunctionalTester $I)
+    public function it_register_a_valide_user(AcceptanceTester $I)
     {
-        $name = 'joe';
-        $email = 'joe@gmail.com';
-        $password = 'secret';
-        $I->amOnRoute('user:register');
-        $I->fillField('username',$name);
-        $I->fillField('email',$email);
-        $I->fillField('password',$password);
-        $I->fillField('password_confirmation',$password);
-        $I->click(Lang::get('form.register'));
-/*        $I->seePageIs(route('home'));*/
-        /*$I->seeInDatabase('users', ['username' => $name, 'email' => $email]);*/
+       $name = 'a';
+       $email = 'a@a.a';
+       $I->register(compact('name','email'));
+       $I->dontSee('username');
+       $I->seeInDatabase('users', ['username' => $name, 'email' => $email]);
     }
 
+    public function it_try_to_register_a_user_with_invalid_inputs(AcceptanceTester $I)
+    {
+        $name = '';
+
+        $I->register(compact('name'));
+
+        $I->see('username');
+    }
+
+    public function it_login_a_user(AcceptanceTester $I)
+    {
+        $I->register();
+        $I->login();
+        $I->dontSee('email');
+    }
+
+    public function it_try_to_login_with_invalid_inputs(AcceptanceTester $I)
+    {
+        $email = 'wrong@email.com';
+        $I->register();
+        $I->login(compact('email'));
+        $I->see('email');
+    }
+
+     public function it_try_to_login_with_unexisted_user(AcceptanceTester $I)
+    {
+        $I->login();
+        $I->see('email');
+    }
 
 }
