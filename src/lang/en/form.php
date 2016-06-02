f6153dc848fd4ea7ad0df89481797f6ae1e54b70
lembarek <lembarek2011@gmail.com>
2016-06-02 20:15:35 +0100

    fix trans in views/login.blade.php

---
+++ src/lang/en/form.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'email' => 'email',
+    'password' => 'password',
+    'login' => 'login',
+    'forget_your_password' => 'forget your password',
+];
--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -11,23 +11,23 @@
 <form method="post">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <div class="form-group">
-        <label class="col-md-4 control-label">{{ Lang::get('form.email')}}</label>
+        <label class="col-md-4 control-label">{{ trans('auth::form.email')}}</label>
         <div class="col-md-6">
         <input type="email" class="form-control" name="email" value="{{ old('email') }}">
         </div>
     </div>
 
     <div class="form-group">
-        <label class="col-md-4 control-label">{{ Lang::get('form.password')}}</label>
+        <label class="col-md-4 control-label">{{ trans('auth::form.password')}}</label>
         <div class="col-md-6">
         <input type="password" class="form-control" name="password" value="{{ old('password') }}">
         </div>
     </div>
 
     <div class="form-group">
-        <input type="submit" value="{{ Lang::get('form.login') }}" class="btn btn-primary" />
+        <input type="submit" value="{{ trans('auth::form.login') }}" class="btn btn-primary" />
     </div>
-    <a href="{{ route('auth::reset.showEmail') }}">forget your password</a>
+    <a href="{{ route('auth::reset.showEmail') }}">{{ trans('auth::form.forget_your_password') }}</a>
 </form>
 @stop
 
