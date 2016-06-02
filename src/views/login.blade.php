2ea8decd78015b50a7b81cdf9d4d810886709b96
lembarek <lembarek2011@gmail.com>
2016-06-02 20:25:59 +0100

    fix div of views/login.blade.php

--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -13,21 +13,23 @@
     <div class="form-group">
         <label class="col-md-4 control-label">{{ trans('auth::form.email')}}</label>
         <div class="col-md-6">
-        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+            <input type="email" class="form-control" name="email" value="{{ old('email') }}">
         </div>
     </div>
 
     <div class="form-group">
         <label class="col-md-4 control-label">{{ trans('auth::form.password')}}</label>
         <div class="col-md-6">
-        <input type="password" class="form-control" name="password" value="{{ old('password') }}">
+            <input type="password" class="form-control" name="password" value="{{ old('password') }}">
         </div>
     </div>
 
     <div class="form-group">
-        <input type="submit" value="{{ trans('auth::form.login') }}" class="btn btn-primary" />
+        <div class="col-md-6 col-md-push-4">
+            <input type="submit" value="{{ trans('auth::form.login') }}" class="btn btn-primary" />
+            <a href="{{ route('auth::reset.showEmail') }}">{{ trans('auth::form.forget_your_password') }}</a>
+        </div>
     </div>
-    <a href="{{ route('auth::reset.showEmail') }}">{{ trans('auth::form.forget_your_password') }}</a>
 </form>
 @stop
 
